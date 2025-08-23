7fd33e0125891a54a601f6f960013265d9c00692
Bruce Hill <bruce@bruce-hill.com>
2025-08-23 19:28:08 -0400

    Autoformat everything with clang-format

--- time_defs.h
+++ time_defs.h
@@ -7,25 +7,26 @@ typedef struct timeval Time;
 
 static OptionalText_t _local_timezone = NONE_TEXT;
 
-static INLINE Text_t num_format(long n, const char *unit)
-{
-    if (n == 0)
-        return Text("now");
-    return Text$from_str(String((int64_t)labs(n), " ", unit, (n == -1 || n == 1) ? "" : "s", n <= 0 ? " ago" : " later"));
+static INLINE Text_t num_format(long n, const char *unit) {
+    if (n == 0) return Text("now");
+    return Text$from_str(
+        String((int64_t)labs(n), " ", unit, (n == -1 || n == 1) ? "" : "s", n <= 0 ? " ago" : " later"));
 }
 
-static void set_local_timezone(Text_t tz)
-{
+static void set_local_timezone(Text_t tz) {
     setenv("TZ", Text$as_c_string(tz), 1);
     _local_timezone = tz;
     tzset();
 }
 
-#define WITH_TIMEZONE(tz, body) ({ if (tz.length >= 0) { \
-        OptionalText_t old_timezone = _local_timezone; \
-        set_local_timezone(tz); \
-        body; \
-        set_local_timezone(old_timezone); \
-    } else { \
-        body; \
-    }})
+#define WITH_TIMEZONE(tz, body)                                                                                        \
+    ({                                                                                                                 \
+        if (tz.length >= 0) {                                                                                          \
+            OptionalText_t old_timezone = _local_timezone;                                                             \
+            set_local_timezone(tz);                                                                                    \
+            body;                                                                                                      \
+            set_local_timezone(old_timezone);                                                                          \
+        } else {                                                                                                       \
+            body;                                                                                                      \
+        }                                                                                                              \
+    })
